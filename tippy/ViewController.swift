284fb3b28e28eb49f25da229bf56c00f7082edcc
Yazmin Carrillo <ycarrillo@csumb.edu>
2020-01-27 15:13:42 -0800

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Yazmin Carrillo on 1/27/20.
+//  Copyright © 2020 Yazmin Carrillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
